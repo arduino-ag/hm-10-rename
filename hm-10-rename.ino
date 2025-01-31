dc6243f132ad9a5dac98b9fdf50c03c0989ff317
baumbus <baumbus_github@icloud.com>
2025-02-01 00:37:36 +0100

    added instructions at the top of the sketch

--- hm-10-rename.ino
+++ hm-10-rename.ino
@@ -1,3 +1,16 @@
+// infos https://forum.ardumower.de/threads/configure-the-hm-10-bluetooth-4-0-module-using-an-arduino-due-board.23860/
+// TL;DR
+// HM10 -> Arduino MEGA
+// RTX -> Pin 18
+// TXD -> Pin 19
+// GND -> GND
+// VCC -> 3.3V
+//
+// Commands over Serial
+// AT+NAMEname -> Sets the name of the device to 'name'
+// AT+NAME -> Returns the name of the device
+// AT -< Returns OK if everything is fine
+
 #include <HardwareSerial.h>
 
 void setup()
@@ -17,7 +30,7 @@ void loop()
         recvChar = Serial1.read();
         Serial.print(recvChar);
     }
-    if (Serial.available())
+    else if (Serial.available())
     {
         recvChar = Serial.read();
         Serial1.write(recvChar);
